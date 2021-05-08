ae0706ad2a04498bf52d311d7bd8229b2dbb8b75
Levi Martines <levi.mferreira@gmail.com>
2021-05-08 11:55:24 -0300

    Removendo interface CommandLineRunner e instanciação dos dados de teste

--- src/main/java/com/levimartines/springboot2essentials/Springboot2EssentialsApplication.java
+++ src/main/java/com/levimartines/springboot2essentials/Springboot2EssentialsApplication.java
@@ -1,8 +1,5 @@
 package com.levimartines.springboot2essentials;
 
-import com.levimartines.springboot2essentials.model.Anime;
-import com.levimartines.springboot2essentials.repository.AnimeRepository;
-import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,24 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springboot2EssentialsApplication implements CommandLineRunner {
 
-    private final AnimeRepository repository;
-
-    public Springboot2EssentialsApplication(AnimeRepository repository) {
-        this.repository = repository;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Springboot2EssentialsApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        repository.saveAll(Arrays.asList(
-            new Anime(null, "Berzeker"),
-            new Anime(null, "Dragon Ball Kai"),
-            new Anime(null, "Dragon Ball Z"),
-            new Anime(null, "Dragon Ball GT"),
-            new Anime(null, "Dragon Ball Super")
-        ));
+
     }
 }
